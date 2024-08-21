9caec8e02c5c506c4a0fa7bc5c02b80c6546d390
pranay-lamse <pranay.l@veerit.com>
2024-08-21 15:14:35 +0530

    added

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,54 +10,55 @@
     <link href="https://erp.erabesa.co.in/uploads/school_content/admin_small_logo/1.png" rel="shortcut icon"
         type="image/x-icon">
 
-    <link rel="stylesheet" href="{{ asset('backend/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/dist/css/jquery.mCustomScrollbar.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/dist/themes/white/skins/_all-skins.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/dist/themes/white/ss-main.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/dist/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/dist/css/jquery.mCustomScrollbar.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/dist/themes/white/skins/_all-skins.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/dist/themes/white/ss-main.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/dist/css/main.css') }}">
 
 
-    <link rel="stylesheet" href="{{ asset('backend/dist/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/dist/css/ionicons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/iCheck/flat/blue.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/morris/morris.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/datepicker/datepicker3.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/colorpicker/bootstrap-colorpicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/dist/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/dist/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/plugins/iCheck/flat/blue.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/plugins/morris/morris.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/plugins/datepicker/datepicker3.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/plugins/colorpicker/bootstrap-colorpicker.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('backend/plugins/daterangepicker/daterangepicker-bs3.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/plugins/daterangepicker/daterangepicker-bs3.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('public/backend/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('backend/dist/css/custom_style.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/datepicker/css/bootstrap-datetimepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/dist/css/custom_style.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/datepicker/css/bootstrap-datetimepicker.css') }}">
     <!--file dropify-->
-    <link rel="stylesheet" href="{{ asset('backend/dist/css/dropify.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/dist/css/dropify.min.css') }}">
     <!--file nprogress-->
-    <link href="{{ asset('backend/dist/css/nprogress.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/backend/dist/css/nprogress.css') }}" rel="stylesheet">
 
     <!--print table-->
-    <link href="{{ asset('backend/dist/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('backend/dist/datatables/css/buttons.dataTables.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('backend/dist/datatables/css/dataTables.bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/backend/dist/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/backend/dist/datatables/css/buttons.dataTables.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/backend/dist/datatables/css/dataTables.bootstrap.min.css') }}" rel="stylesheet">
     <!--print table mobile support-->
-    <link href="{{ asset('backend/dist/datatables/css/responsive.dataTables.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('backend/dist/datatables/css/rowReorder.dataTables.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/backend/dist/datatables/css/responsive.dataTables.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/backend/dist/datatables/css/rowReorder.dataTables.min.css') }}" rel="stylesheet">
     <!--language css-->
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/0.8.2/css/flag-icon.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/dist/css/bootstrap-select.min.css') }}">
-    <script src="{{ asset('backend/custom/jquery.min.js') }}"></script>
-    <script src="{{ asset('backend/dist/js/moment.min.js') }}"></script>
-    <script src="{{ asset('backend/datepicker/js/bootstrap-datetimepicker.js') }}"></script>
-    <script src="{{ asset('backend/plugins/colorpicker/bootstrap-colorpicker.js') }}"></script>
-    <script src="{{ asset('backend/datepicker/date.js') }}"></script>
-    <script src="{{ asset('backend/dist/js/jquery-ui.min.js') }}"></script>
-    <script src="{{ asset('backend/js/school-custom.js') }}"></script>
-    <script src="{{ asset('backend/js/school-admin-custom.js') }}"></script>
-    <script src="{{ asset('backend/js/sstoast.js') }}"></script>
+    <link rel="stylesheet" type="text/css" href="{{ asset('public/backend/dist/css/bootstrap-select.min.css') }}">
+    <script src="{{ asset('public/backend/custom/jquery.min.js') }}"></script>
+    <script src="{{ asset('public/backend/dist/js/moment.min.js') }}"></script>
+    <script src="{{ asset('public/backend/datepicker/js/bootstrap-datetimepicker.js') }}"></script>
+    <script src="{{ asset('public/backend/plugins/colorpicker/bootstrap-colorpicker.js') }}"></script>
+    <script src="{{ asset('public/backend/datepicker/date.js') }}"></script>
+    <script src="{{ asset('public/backend/dist/js/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('public/backend/js/school-custom.js') }}"></script>
+    <script src="{{ asset('public/backend/js/school-admin-custom.js') }}"></script>
+    <script src="{{ asset('public/backend/js/sstoast.js') }}"></script>
 
-    <link rel="stylesheet" href="{{ asset('backend/fullcalendar/dist/fullcalendar.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/fullcalendar/dist/fullcalendar.print.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('public/backend/fullcalendar/dist/fullcalendar.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/backend/fullcalendar/dist/fullcalendar.print.min.css') }} ">
     <script type="text/javascript">
         var baseurl = @json(url());
         var start_week = 1;
