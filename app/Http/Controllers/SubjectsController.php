35655e02f0b3306ea99fe91b17a6925dfd651540
pranay-lamse <pranay.l@veerit.com>
2024-09-06 17:05:23 +0530

    added

--- app/Http/Controllers/SubjectsController.php
+++ app/Http/Controllers/SubjectsController.php
@@ -51,29 +51,30 @@ class SubjectsController extends Controller
 
         // Validate the incoming request
         $validatedData = $request->validate([
-            'house_name' => 'required|string|max:255',
+            'name' => 'required|string|max:255',
         ]);
 
         // Check if the category already exists in the Category model
-        $existingCategory = Subjects::where('house_name', $validatedData['house_name'])->first();
+        $existingCategory = Subjects::where('name', $validatedData['name'])->first();
 
         if ($existingCategory) {
             return response()->json([
                 'success' => false,
-                'message' => 'house name already exists',
+                'message' => 'Subjects name already exists',
             ], 409); // 409 Conflict status code
         }
 
         // Create a new category
         $category = new Subjects();
-        $category->house_name = $validatedData['house_name'];
-        $category->description = $request->description;
-        $category->is_active = 0;
+        $category->name = $validatedData['name'];
+        $category->code = $request->code;
+        $category->type = $request->type;
+        $category->is_active =  $request->is_active ? $request->is_active : 0;
         $category->save();
 
         return response()->json([
             'success' => true,
-            'message' => 'house name saved successfully',
+            'message' => 'Subjects name saved successfully',
             'category' => $category,
         ], 201); // 201 Created status code
     }
@@ -111,20 +112,20 @@ class SubjectsController extends Controller
     public function update(Request $request,string $id)
     {
 
-        // Find the house_name by id
+        // Find the Subjects_name by id
         $category = Subjects::findOrFail($id);
 
        // Validate only the fields you need to validate
             $validatedData = $request->validate([
-                'house_name' => 'required',
+                'name' => 'required',
             ]);
 
             // Get the description from the request without validation
-            $description = $request->input('description');
+            $name = $request->input('name');
 
             // Merge the validated data with the description
             $updatedData = array_merge($validatedData, [
-                'description' => $description,
+                'name' => $name,
             ]);
 
         // Update the category
@@ -153,10 +154,10 @@ class SubjectsController extends Controller
             $category->delete();
 
             // Return success response
-            return response()->json(['success' => true, 'message' => 'house name deleted successfully']);
+            return response()->json(['success' => true, 'message' => 'Subjects name deleted successfully']);
         } catch (\Exception $e) {
             // Handle failure (e.g. if the category was not found)
-            return response()->json(['success' => false, 'message' => 'house name deletion failed: ' . $e->getMessage()], 500);
+            return response()->json(['success' => false, 'message' => 'Subjects name deletion failed: ' . $e->getMessage()], 500);
         }
     }
 }
