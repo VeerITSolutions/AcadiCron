da27bf1c929e75d132bcf7d47eb813654d47f580
pranay-lamse <pranay.l@veerit.com>
2024-09-10 12:33:58 +0530

    added

--- app/Http/Controllers/StaffLeaveRequestController.php
+++ app/Http/Controllers/StaffLeaveRequestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\StaffLeaveRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -61,10 +62,36 @@ class StaffLeaveRequestController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
+    public function create(Request $request){
+
+
+        // Validate the incoming request
+        $validatedData = $request->all();
+
+
+
+        // Create a new category
+        $category = new StaffLeaveRequest();
+        $category->staff_id  = $validatedData['staff_id '];
+        $category->leave_type_id   = $validatedData['leave_type_id  '];
+        $category->leave_from  = $validatedData['leave_from '];
+        $category->leave_to  = $validatedData['leave_to'];
+        $category->leave_days  = $validatedData['leave_days'];
+        $category->employee_remark  = $validatedData['employee_remark'];
+        $category->admin_remark  = $validatedData['admin_remark'];
+        $category->status  = $validatedData['status'];
+        $category->applied_by  = $validatedData['applied_by'];
+        $category->document_file  = $validatedData['document_file'];
+        $category->date  = $validatedData['date'];
+        $category->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Staff Leave saved successfully',
+            'category' => $category,
+        ], 201); // 201 Created status code
     }
+
 
     /**
      * Store a newly created resource in storage.
@@ -93,16 +120,47 @@ class StaffLeaveRequestController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+
+
+    public function update(Request $request,string $id)
     {
-        //
+
+        // Find the category by id
+        $category = StaffLeaveRequest::findOrFail($id);
+
+        // Validate the request data
+        $validatedData = $request->all();
+
+        // Update the category
+        $category->update($validatedData);
+
+
+
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Edit successfully',
+            'category' => $category,
+        ], 201); // 201 Created status code
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        try {
+            // Find the category by ID
+            $category = StaffLeaveRequest::findOrFail($id);
+
+            // Delete the category
+            $category->delete();
+
+            // Return success response
+            return response()->json(['success' => true, 'message' => 'Staff Leave deleted successfully']);
+        } catch (\Exception $e) {
+            // Handle failure (e.g. if the category was not found)
+            return response()->json(['success' => false, 'message' => 'Staff Leave  deletion failed: ' . $e->getMessage()], 500);
+        }
     }
 }
