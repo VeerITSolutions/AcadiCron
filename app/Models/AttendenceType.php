ebe03e2393b8f1653c9716e985904bee10e3f1f1
pranay-lamse <pranay.l@veerit.com>
2025-01-06 16:57:42 +0530

    added

--- app/Models/AttendenceType.php
+++ app/Models/AttendenceType.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class AttendenceType extends Model
 {
     use HasFactory;
+    protected $table = 'attendence_type';
+    protected $fillable = ['type','key_value','is_active', 'created_at','updated_at'];
 }
