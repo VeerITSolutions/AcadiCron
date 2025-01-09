8865aeb4b929ffd8c96e5f0d4a8fbd4126e81c69
ManaliC0712 <manali.c@veerit.com>
2025-01-09 17:43:25 +0530

    Added

--- app/Models/AlumniEvents.php
+++ app/Models/AlumniEvents.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class AlumniEvents extends Model
 {
     use HasFactory;
+
+    protected $table = 'alumni_events';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+       'title',
+        'event_for',
+        'session_id',
+        'class_id',
+        'section',
+        'from_date',
+        'to_date',
+        'note',
+        'photo',
+        'is_active',
+        'event_notification_message',
+        'show_onwebsite',
+    ];
 }
