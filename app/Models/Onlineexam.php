5689574994cab9bd2c34d74b541afc225570ea8a
nilesh.b@veerit.com <rahul.u@veerit.com>
2025-01-09 19:44:27 +0530

    online exam

--- app/Models/Onlineexam.php
+++ app/Models/Onlineexam.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Onlineexam extends Model
 {
     use HasFactory;
+
+
+    protected $table = 'onlineexam';
+
+    protected $fillable = ['exam', 'attempt', 'exam_from', 'exam_to', 'is_quiz', 'auto_publish_date', 'time_from', 'time_to', 'duration', 'passing_percentage', 'duration', 'description', 'session_id', 'publish_result', 'is_active', 'is_marks_display', 'is_neg_marking', 'is_random_question', 'is_rank_generated', 'publish_exam_notification', 'publish_result_notification', 'created_at', 'updated_at'];
 }
