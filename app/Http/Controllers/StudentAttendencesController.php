af87a30f962bab51077f634c92a583926b990a51
pranay-lamse <pranay.l@veerit.com>
2025-02-19 12:14:39 +0530

    added

--- app/Http/Controllers/StudentAttendencesController.php
+++ app/Http/Controllers/StudentAttendencesController.php
@@ -229,7 +229,10 @@ class StudentAttendencesController extends Controller
         $session_id = $validated['session_id'];
         $student_session_id = StudentSession::where('student_id', $student_id)->where('session_id', $session_id)->first();
 
-        $attendance = StudentAttendences::with('attendanceType')->where('student_session_id', $student_session_id->id)
+        $attendance = StudentAttendences::with('attendanceType')
+            ->where('student_session_id', $student_session_id->id)
+            ->whereYear('date', $year) // Filter by year
+            ->whereMonth('date', $month) // Filter by month
             ->get();
 
         return response()->json([
