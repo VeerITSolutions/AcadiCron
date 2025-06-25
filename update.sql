49a4ff771f48c8d44c6d83539ee0f255c62568e6
pranay-lamse <pranay.l@veerit.com>
2025-06-25 10:58:08 +0530

    added

--- update.sql
+++ update.sql
@@ -222,3 +222,17 @@ CREATE TABLE classwork (
   PRIMARY KEY (id),
   KEY subject_group_subject_id (subject_group_subject_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `personal_access_tokens` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `tokenable_type` VARCHAR(255) NOT NULL,
+  `tokenable_id` BIGINT UNSIGNED NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `token` VARCHAR(64) NOT NULL,
+  `abilities` TEXT,
+  `last_used_at` TIMESTAMP NULL,
+  `expires_at` TIMESTAMP NULL,
+  `created_at` TIMESTAMP NULL,
+  `updated_at` TIMESTAMP NULL,
+  INDEX `tokenable_type_tokenable_id_index` (`tokenable_type`, `tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
